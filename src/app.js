26f2d9358b548712b399c5452f84ce9e43cc7ed9
Agrim Agrawal <agrimagrawal2479@gmail.com>
2026-03-24 04:30:03 +0530

    implement playlist functionality; add playlist controller with CRUD operations and integrate playlist routes in app

--- src/app.js
+++ src/app.js
@@ -24,15 +24,15 @@ import userRouter from './routes/user.routes.js'
 import videoRouter from './routes/video.route.js'
 import commentRouter from './routes/comment.route.js'
 import tweetRouter from './routes/tweet.route.js'
+import playlistRouter from './routes/playlist.route.js'
 // import likeRouter from './routes/like.route.js'
-// import playlistRouter from './routes/playlist.route.js'
 
 // routes declaration
 app.use("/api/v1/users",userRouter);
 app.use("/api/v1/video",videoRouter);
 app.use("/api/v1/comments",commentRouter);
 app.use("/api/v1/tweet",tweetRouter);
+app.use("/api/v1/playlist",playlistRouter);
 // app.use("/api/v1/like",likeRouter);
-// app.use("/api/v1/playlist",playlistRouter);
 
 export {app}
