d1f9eb55cf0887efb24364083c64d84d473cfc54
Agrim Agrawal <agrimagrawal2479@gmail.com>
2026-03-28 02:51:05 +0530

    implement like functionality; add like controller with toggle and retrieval methods, and integrate like routes in app

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ import videoRouter from './routes/video.route.js'
 import commentRouter from './routes/comment.route.js'
 import tweetRouter from './routes/tweet.route.js'
 import playlistRouter from './routes/playlist.route.js'
-// import likeRouter from './routes/like.route.js'
+import likeRouter from './routes/like.route.js'
 
 // routes declaration
 app.use("/api/v1/users",userRouter);
@@ -33,6 +33,6 @@ app.use("/api/v1/video",videoRouter);
 app.use("/api/v1/comments",commentRouter);
 app.use("/api/v1/tweet",tweetRouter);
 app.use("/api/v1/playlist",playlistRouter);
-// app.use("/api/v1/like",likeRouter);
+app.use("/api/v1/like",likeRouter);
 
 export {app}
